dd73452a0622a4c6a3f888bde93ee6c0de6aeb6e
frankmolenaar <frank.molenaar@ogd.nl>
2015-05-27 10:39:28 +0200

    initial commit

---
+++ DressToImpress/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DressToImpress.Startup))]
+namespace DressToImpress
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ iAmDressedToKill/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iAmDressedToKill.MvcApplication" Language="C#" %>
