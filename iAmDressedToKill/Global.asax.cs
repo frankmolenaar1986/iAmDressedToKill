31d7496b89224518e7d5d7f4faf93b15f269cb1b
frankmolenaar1986 <frankmolenaarma@gmail.com>
2015-06-13 12:16:10 +0200

    bam bam

--- iAmDressedToKill/Global.asax.cs
+++ iAmDressedToKill/Global.asax.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using iAmDressedToKill.Models;
 
 namespace iAmDressedToKill
 {
@@ -13,6 +11,7 @@ namespace iAmDressedToKill
     {
         protected void Application_Start()
         {
+			//Database.SetInitializer(new DropCreateDatabaseAlways<DressToImpress>());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ iAmDressedToKill/Models/DressModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace iAmDressedToKill.Models
+{
+	public class DressModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Color { get; set; }
+		public decimal Price { get; set; }
+		public string ImageUrl { get; set; }
+	}
+
+	public class DressToImpress : DbContext
+	{
+		public DbSet<DressModel> Movies { get; set; }
+	}
+}
